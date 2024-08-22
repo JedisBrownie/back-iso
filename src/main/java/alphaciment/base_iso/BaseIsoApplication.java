140c59954dee96feb2d27e0c013e51a7217c8310
Ismael-Raveloson <isma.raveloson@gmail.com>
2024-08-22 11:55:55 +0300

    init(2)

--- src/main/java/alphaciment/base_iso/BaseIsoApplication.java
+++ src/main/java/alphaciment/base_iso/BaseIsoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackIsoApplication {
+public class BaseIsoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackIsoApplication.class, args);
+		SpringApplication.run(BaseIsoApplication.class, args);
 	}
 
 }
